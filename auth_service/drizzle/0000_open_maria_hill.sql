922c05a87210e89b22e0c3ccbeb55c41b7d116c4
Tim <tim8912097887@gmail.com>
2026-05-13 07:38:37 +0800

    ci(auth_service): add workflow file for auth_service

---
+++ auth_service/drizzle/0000_open_maria_hill.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"username" varchar(255) NOT NULL,
+	"email" varchar(50) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"is_verified" boolean DEFAULT false NOT NULL,
+	"login_lock" timestamp with time zone,
+	"login_attempt" integer DEFAULT 0 NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"deleted_at" timestamp with time zone,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
