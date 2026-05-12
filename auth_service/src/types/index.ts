10f29e6e6cd2f10ec268cfa66dc04eb529856dd5
Tim <tim8912097887@gmail.com>
2026-05-12 08:36:26 +0800

    feat(auth_service): add custom error and errorHandler

---
+++ auth_service/src/types/index.ts
@@ -0,0 +1,60 @@
+import { Response } from 'express';
+
+export enum ERROR_CODE {
+    BAD_REQUEST = 400,
+    NOT_FOUND = 404,
+    UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
+    SERVER_ERROR = 500,
+    SERVER_CONFLICT = 409,
+    TOO_MANY_REQUEST = 429,
+    SERVER_UNAVAILABLE = 503,
+}
+
+export type State = 'success' | 'error' | 'redirect';
+
+export type ErrorObject = {
+    status: string;
+    code: ERROR_CODE;
+    detail: string;
+    errors?: ValidationError[];
+};
+
+export type Data = null | any;
+
+export type Params = {
+    state: State;
+    data?: Data;
+    error?: ErrorObject;
+};
+export type ResponseStructure = {
+    state: State;
+    error: ErrorObject | null;
+    data: Data | null;
+    meta: {
+        timestamp: string;
+    };
+};
+
+export type SuccessResponse<T> = {
+    res: Response;
+    data: T;
+    statusCode?: number;
+};
+
+export type ValidationError = {
+    field: string;
+    value: string;
+};
+
+export type Payload = {
+    sub: string;
+    token_version: number;
+};
+
+export type AuthPayload = {
+    sub: string;
+    token_version: number;
+    iat: number;
+    exp: number;
+};
