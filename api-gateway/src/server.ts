39efe97d83e3e3f0606ac21803edc4e9df975ad1
Tim <tim8912097887@gmail.com>
2026-05-16 10:16:46 +0800

    feat(api-gateway): setup server and subscribe graceful shutdown

---
+++ api-gateway/src/server.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import morgan from 'morgan';
+import { errorHandler } from '#middlewares/error-handler.js';
+import { notFoundHandler } from '#middlewares/not-found-handler.js';
+
+export const initializeApp = () => {
+    const app = express();
+    // Body parser middleware
+    app.use(express.json());
+
+    // HTTP request logger middleware
+    app.use(
+        morgan(':method :url :status :res[content-length] - :response-time ms'),
+    ); // Log to console
+
+    // Healthy check endpoint
+    app.get('/health', (_req, res) => {
+        res.status(200).json({
+            status: 'OK',
+            service: 'Api-Gateway_Service',
+            timestamp: new Date().toISOString(),
+        });
+    });
+
+    // Error Handler
+    app.use(errorHandler);
+    app.use(notFoundHandler);
+    return app;
+};
